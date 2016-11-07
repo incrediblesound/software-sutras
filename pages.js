58ad5ffdbb8c93e7ae97228ce94e10f1ec0b8808
James Edwards <jamese@syapse.com>
2016-11-07 08:40:22 -0800

    add layout for tentative course structure

--- pages.js
+++ pages.js
@@ -1,4 +1,10 @@
 module.exports = {
   '/about': '/static/about.html',
   '/version-control': '/static/version_control.html'
+  '/dev-environment': '/static/dev_env.html',
+  '/algorithms': '/static/algorithms.html',
+  '/mvc': '/static/mvc.html',
+  '/react': '/static/react.html',
+  '/redux': '/static/redux.html',
+  '/architecture': '/static/architecture.html'
 }
